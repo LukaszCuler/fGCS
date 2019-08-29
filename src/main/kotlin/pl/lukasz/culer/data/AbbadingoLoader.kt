983dff968fb4cf4cca6c664db691472e69f8b989
l.culer <l.culer@bsgroup.eu>
2019-08-29 16:41:09 +0200

    AbbadingoLoader finish

--- src/main/kotlin/pl/lukasz/culer/data/AbbadingoLoader.kt
+++ src/main/kotlin/pl/lukasz/culer/data/AbbadingoLoader.kt
@@ -1,13 +1,60 @@
 package pl.lukasz.culer.data
 
+import java.io.IOException
+import jdk.internal.net.http.common.Utils.close
+import pl.lukasz.culer.fuzzy.IntervalFuzzyNumber
+import pl.lukasz.culer.utils.ABBADINGO_INVALID_EXAMPLE
+import pl.lukasz.culer.utils.ABBADINGO_INVALID_HEADER
+import pl.lukasz.culer.utils.Logger
+import java.io.FileReader
+import java.io.BufferedReader
+
+
+
 class AbbadingoLoader {
     companion object {
-        val L1 = "./TestSets/l1.txt"
-        val L2 = "./TestSets/l2.txt"
-        val L5 = "./TestSets/l5.txt"
-        val TOY = "./TestSets/toy.txt"
-        val AnBn = "./TestSets/AnBn.txt"
+        const val L1 = "./TestSets/l1.txt"
+        const val L2 = "./TestSets/l2.txt"
+        const val L5 = "./TestSets/l5.txt"
+        const val TOY = "./TestSets/toy.txt"
+        const val AnBn = "./TestSets/AnBn.txt"
+
+        const val TAG = "AbbadingoLoader"
 
 
+        fun loadAbbadingoTestSet(filePath : String) : List<TestExample> {
+            val reader: BufferedReader
+            try {
+                reader = BufferedReader(FileReader(filePath))
+                val firstLine: String = reader.readLine()
+
+                //general parameters of file
+                val firstLineValues = firstLine.split(' ')
+                if(firstLineValues.isEmpty()) return returnWithError(ABBADINGO_INVALID_HEADER.format(filePath))
+
+                val numberOfExamples = firstLineValues[0].toIntOrNull() ?: return returnWithError(ABBADINGO_INVALID_HEADER.format(filePath))
+
+                val testSet : MutableList<TestExample> = mutableListOf()
+                for(i in 1..numberOfExamples){
+                    val exampleLine = reader.readLine() ?: return returnWithError(ABBADINGO_INVALID_EXAMPLE.format(i, filePath), testSet)
+                    val exampleSegments = exampleLine.split(' ')
+                    if(exampleSegments.size < 2) return returnWithError(ABBADINGO_INVALID_EXAMPLE.format(i, filePath), testSet)
+
+                    val membershipValue = exampleSegments[0].toDoubleOrNull() ?: return returnWithError(ABBADINGO_INVALID_EXAMPLE.format(i, filePath), testSet)
+                    val sequence = exampleSegments.subList(1, exampleSegments.lastIndex).joinToString().toLowerCase()
+                    testSet.add(TestExample(sequence, IntervalFuzzyNumber(membershipValue)))
+                }
+                reader.close()
+                return testSet
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return listOf()
+            }
+        }
+
+        private fun returnWithError(message : String, testSet : List<TestExample> = listOf()) : List<TestExample> {
+            Logger.instance.e(TAG, message)
+            return testSet
+        }
     }
 }
---
+++ src/main/kotlin/pl/lukasz/culer/utils/ErrorMessages.kt
@@ -0,0 +1,4 @@
+package pl.lukasz.culer.utils
+
+const val ABBADINGO_INVALID_HEADER = "Invalid header in file %s!"
+const val ABBADINGO_INVALID_EXAMPLE = "Error in loading example %i from file %s!"
