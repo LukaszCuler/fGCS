8ee3645b996e901545d85f8311ec760be652aef9
l.culer <l.culer@bsgroup.eu>
2019-09-16 19:24:12 +0200

    unit tests fixed

--- src/test/kotlin/GrammarControllerTests.kt
+++ src/test/kotlin/GrammarControllerTests.kt
@@ -22,6 +22,6 @@ class GrammarControllerTests {
         Assert.assertEquals(3, gc.grammar.tSymbols.size) //good number of terminals
         Assert.assertEquals(4, gc.grammar.nSymbols.size)  //good number of non-terminals (start+each for terminal)
         Assert.assertNotNull(gc.grammar.starSymbol)             //we have start symbol
-        Assert.assertEquals(gc.grammar.nSymbols.single(), gc.grammar.starSymbol)        //and they are the same!
+        Assert.assertTrue(gc.grammar.nSymbols.contains(gc.grammar.starSymbol))        //and it's present in non terminals
     }
 }
