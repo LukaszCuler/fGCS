e299a0f5f96f97caa6f22a4ff7fa1a8cc511880e
l.culer <l.culer@bsgroup.eu>
2019-09-19 10:06:31 +0200

    CYK impl

--- src/test/kotlin/CYKControllerTests.kt
+++ src/test/kotlin/CYKControllerTests.kt
@@ -1,6 +1,12 @@
+import org.junit.Assert
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.runners.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class CYKControllerTests {
+    @Test
+    fun mockTest(){
+        Assert.assertTrue(true)
+    }
 }
