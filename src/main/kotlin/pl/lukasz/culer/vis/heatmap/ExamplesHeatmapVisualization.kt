6a85bda1e20ccbbfe79e5ed978ae2a319d84d438
l.culer <l.culer@bsgroup.eu>
2019-10-29 16:14:26 +0100

    ExamplesHeatmapVisualization

---
+++ src/main/kotlin/pl/lukasz/culer/vis/heatmap/ExamplesHeatmapVisualization.kt
@@ -0,0 +1,13 @@
+package pl.lukasz.culer.vis.heatmap
+
+import pl.lukasz.culer.fgcs.FGCS
+
+class ExamplesHeatmapVisualization(val listToVisualize : List<FGCS.ExampleAnalysisResult>){
+    /**
+     * region public methods
+     */
+    fun performAnalysis() {
+
+    }
+    //endregion
+}
