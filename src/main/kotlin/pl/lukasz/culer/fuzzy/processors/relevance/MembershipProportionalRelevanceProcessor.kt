05e1c55b745ee0443b6f0a92e0a14c7dc63073f5
l.culer <l.culer@bsgroup.eu>
2019-12-08 23:26:22 +0100

    relevance normalization

--- src/main/kotlin/pl/lukasz/culer/fuzzy/processors/relevance/MembershipProportionalRelevanceProcessor.kt
+++ src/main/kotlin/pl/lukasz/culer/fuzzy/processors/relevance/MembershipProportionalRelevanceProcessor.kt
@@ -3,6 +3,7 @@ package pl.lukasz.culer.fuzzy.processors.relevance
 import pl.lukasz.culer.fgcs.models.trees.MultiParseTreeNode
 import pl.lukasz.culer.fuzzy.IntervalFuzzyNumber
 import pl.lukasz.culer.fuzzy.processors.relevance.base.RelevanceProcessor
+import pl.lukasz.culer.utils.Consts.Companion.FULL_RELEVANCE
 import pl.lukasz.culer.utils.Consts.Companion.NOT_RELEVANT_AT_ALL
 
 /**
@@ -13,15 +14,18 @@ class MembershipProportionalRelevanceProcessor : RelevanceProcessor {
     override fun assignRelevanceToVariants(children: MutableList<MultiParseTreeNode.SubTreePair>) {
         //first we find the greatest value
         children.sortBy { it.classificationMembership }
-        val maxValue = children.last().classificationMembership.midpoint
+        val maxItem = children.maxBy { it.classificationMembership }?.classificationMembership?.midpoint ?: 0.0
+        val minItem = children.minBy { it.classificationMembership }?.classificationMembership?.midpoint ?: 0.0
 
-        if(maxValue != 0.0){       //we have to be protected in case of strange things
+        if(maxItem==minItem) {
             for(variant in children){
-                variant.relevance = IntervalFuzzyNumber(variant.classificationMembership.midpoint / maxValue)
+                variant.relevance = if(maxItem == 0.0) NOT_RELEVANT_AT_ALL else FULL_RELEVANCE
             }
         } else {
+            val coef = 1.0/(maxItem-minItem)
             for(variant in children){
-                variant.relevance = NOT_RELEVANT_AT_ALL
+                variant.relevance = variant.classificationMembership - minItem
+                variant.relevance = variant.relevance * coef
             }
         }
     }
