e06046d18a11539b7fc9fd2acbc29c04daa04c61
l.culer <l.culer@bsgroup.eu>
2020-05-02 14:11:51 +0200

    SimpleOccurrenceMembershipAssigner impl

--- src/main/kotlin/pl/lukasz/culer/fgcs/rules/SimpleOccurrenceMembershipAssigner.kt
+++ src/main/kotlin/pl/lukasz/culer/fgcs/rules/SimpleOccurrenceMembershipAssigner.kt
@@ -1,50 +1,71 @@
 package pl.lukasz.culer.fgcs.rules
 
 import pl.lukasz.culer.fgcs.FGCS
-import pl.lukasz.culer.fgcs.models.Grammar
 import pl.lukasz.culer.fgcs.models.rules.NRule
 import pl.lukasz.culer.fgcs.rules.base.MembershipAssigner
-import java.util.concurrent.ConcurrentMap
-import java.util.concurrent.atomic.AtomicLong
+import pl.lukasz.culer.fuzzy.F
+import pl.lukasz.culer.utils.RxUtils
+import java.util.concurrent.atomic.DoubleAdder
 
+/**
+ * Simple -> doesn't consider type-2
+ */
+//@TODO UT
 class SimpleOccurrenceMembershipAssigner : MembershipAssigner() {
     companion object {
-        const val POSITIVE_OCCURED = "POSITIVE_OCCURRED"
-        const val NEGATIVE_OCCURED = "NEGATIVE_OCCURRED"
+        const val POSITIVE_ACCUMULATED = "POSITIVE_ACCUMULATED"
+        const val NEGATIVE_ACCUMULATED = "NEGATIVE_ACCUMULATED"
     }
 
-    //extension properties
-    fun NRule.initializePositiveOccurred() {
-        analysisTemps[POSITIVE_OCCURED] = AtomicLong()
+    //region extension properties
+    private fun NRule.initializePositiveAccs() {
+        analysisTemps[POSITIVE_ACCUMULATED] = DoubleAdder()
     }
 
-    fun NRule.getPositiveOccurred() = analysisTemps[POSITIVE_OCCURED]
+    fun NRule.getPositiveAccs() = analysisTemps[POSITIVE_ACCUMULATED] as DoubleAdder
 
-    fun NRule.initializeNegativeOccurred() {
-        analysisTemps[NEGATIVE_OCCURED] = AtomicLong()
+    private fun NRule.initializeNegativeAccs() {
+        analysisTemps[NEGATIVE_ACCUMULATED] = DoubleAdder()
     }
 
-    fun NRule.getNegativeOccurred() = analysisTemps[NEGATIVE_OCCURED]
-
+    fun NRule.getNegativeAccs() = analysisTemps[NEGATIVE_ACCUMULATED] as DoubleAdder
+    //endregion
+    //region overrides
     override fun initialize() {
         grammarController?.grammar?.nRules?.forEach {
-            it.initializePositiveOccurred()
-            it.initializeNegativeOccurred()
+            it.initializePositiveAccs()
+            it.initializeNegativeAccs()
         }
     }
 
     override fun analyzeExample(example: FGCS.ExampleAnalysisResult) {
+        val occurredRules = mutableSetOf<NRule>()
         parseTreeController?.processRootToNodesAll(example.multiParseTreeNode) {node ->
             node.subtrees.forEach { variant ->
                 grammarController?.nRulesWith(node.node, variant.subTreePair.first.node, variant.subTreePair.second.node)
                     ?.single()
-                    ?.occurredInParsing
-                    ?.set(true)
+                    ?.let { occurredRules.add(it) }
             }
+        }
+        val posAdd = example.fuzzyClassification.midpoint
+        val negAdd = 1.0 - posAdd
+        occurredRules.forEach {
+            it.getPositiveAccs().add(posAdd)
+            it.getNegativeAccs().add(negAdd)
         }
     }
 
     override fun finalize() {
-        TODO("Not yet implemented")
+        //we translate occurances into membership
+        val nRules = grammarController?.grammar?.nRules?.toList() ?: return
+
+        RxUtils.computeParallelly(nRules) {
+            val positive = it.getPositiveAccs().sum()
+            val sum = positive + it.getNegativeAccs().sum()
+            it.membership = F(positive/sum)
+        }
     }
+    //endregion
+    //region private stuff
+    //endregion
 }
